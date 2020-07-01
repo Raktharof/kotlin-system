e103b7d0c017e04bdcad7a8762d3fe97fbdee22b
raktharof <raktharof@gmail.com>
2020-07-01 11:28:59 -0300

    CNPJ class
    Added
    - CNPJ class with function for format and value validations

---
+++ src/Classes/cnpjClass.kt
@@ -0,0 +1,58 @@
+package Classes
+
+class CNPJ(cnpj:String){
+    val cnpj = cnpj
+
+    fun checkFormatAndValue() :String {
+        if (cnpj.length == 14 || (cnpj.length == 18 && ((cnpj[2] == '.' && cnpj[6] == '.' && cnpj[10] == '/' && cnpj[15] == '-')))) {
+            val re = Regex("[^0-9]")
+            val cnpjFormatted = re.replace(cnpj, "")
+            return checkValidCNPJ(cnpjFormatted)
+        } else {
+            return "Formato de CNPJ incorreto. Por favor, insira formato correto. " +
+                    "Ex: XXXXXXXXXXXXXX ou XX.XXX.XXX/XXXX-XX"
+        }
+    }
+
+    private fun checkValidCNPJ(cpfNum:String):String {
+        val invalidValueCheck = (cpfNum == "00000000000000") || (cpfNum == "11111111111111") || (cpfNum == "22222222222222") ||
+                (cpfNum == "33333333333333") || (cpfNum == "44444444444444") || (cpfNum == "55555555555555") || (cpfNum == "66666666666666") ||
+                (cpfNum == "77777777777777") || (cpfNum == "88888888888888") || (cpfNum == "99999999999999") || (cpfNum.length < 14)
+        var firstDigitCheck = false
+        var secondDigitCheck = false
+
+        if (!invalidValueCheck) {
+            val digits = cpfNum.map { Integer.parseInt(it.toString()) }.toIntArray()
+
+            val firstDigitValue = (digits[0] * 5 + digits[1] * 4 + digits[2] * 3 + digits[3] * 2 + digits[4] * 9 +
+                    digits[5] * 8 + digits[6] * 7 + digits[7] * 6 + digits[8] * 5 + digits[9] * 4 + digits[10] * 3
+                    + digits[11] * 2) % 11
+
+            if (firstDigitValue > 1) {
+                firstDigitCheck = (11 - firstDigitValue) == digits[12]
+            } else {
+                firstDigitCheck = digits[12] == 0
+            }
+
+            if (firstDigitCheck) {
+                val secondDigitValue = (digits[0] * 6 + digits[1] * 5 + digits[2] * 4 + digits[3] * 3 + digits[4] * 2 +
+                        digits[5] * 9 + digits[6] * 8 + digits[7] * 7 + digits[8] * 6 + digits[9] * 5 + digits[10] * 4
+                        + digits[11] * 3 + digits[12] * 2) % 11
+
+                if (secondDigitValue > 1) {
+                    secondDigitCheck = (11 - secondDigitValue) == digits[13]
+                } else {
+                    secondDigitCheck = digits[13] == 0
+                }
+            }
+        }
+
+        if (!invalidValueCheck && firstDigitCheck && secondDigitCheck){
+            return "CNPJ $cnpj Válido"
+        } else {
+            return "CNPJ Inválido"
+        }
+
+    }
+
+}
--- src/app.kt
+++ src/app.kt
@@ -1,6 +1,8 @@
+import Classes.CNPJ
 import Classes.Rectangle
 
 fun main() {
+    val filledCNPJ = CNPJ("31.605.522/0001-40")
     val squareA = Rectangle(3, 11, 5, 11)
     val squareB = Rectangle(7, 13, 2, 7)
     val squareC = Rectangle(10, 14, 10, 13)
@@ -8,5 +10,7 @@ fun main() {
     println(squareA.intersects(squareB))
     println(squareA.intersects(squareC))
     println(squareB.intersects(squareC))
+    println(filledCNPJ.checkFormatAndValue())
+    println(filledCNPJ.cnpj)
 }
 
